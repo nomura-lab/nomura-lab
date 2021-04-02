a45d51c8f0eb4b077560f017eb5eb35628d78169
nomura-lab <81790348+nomura-lab@users.noreply.github.com>
2021-04-02 16:23:52 +0900

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -10,10 +10,12 @@
 		reset: 'full',
 		breakpoints: {
 			global:		{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 50 } },
-			wide:		{ range: '-1680', href: 'css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-			normal:		{ range: '-1280', href: 'css/style-normal.css', containers: 960, lockViewport: true },
+			wide:		{ range: '-1680', href: 'css/style-wide.css', containers: 1300, grid: { gutters: 40 } },
+			normal:		{ range: '-1320', href: 'css/style-normal.css', containers: 960, lockViewport: true },
 			narrow:		{ range: '-980', href: 'css/style-narrow.css', containers: '95%', grid: { gutters: 25 } },
-			narrower:	{ range: '-1020', href: 'css/style-narrower.css', grid: { collapse: true } },
+			/*narrower:	{ range: '-1020', href: 'css/style-narrower.css', grid: { collapse: true } },*/
+			narrower:	{ range: '-1020', href: 'css/style-narrower.css', containers: '90%', grid: { gutters: 25 } },
+			narrower2:	{ range: '-700', href: 'css/style-narrower.css', grid: { collapse: true } },
 			mobile:		{ range: '-640', href: 'css/style-mobile.css', containers: '90%', grid: { gutters: 15 } }
 		}
 	}, {
